e1cf870970b6259e0e3dc65588010936292ee587
josmel <josmelvt@gmail.com>
2024-12-09 17:02:04 -0500

    servicio email

--- resources/views/emails/reclamo_registrado.blade.php
+++ resources/views/emails/reclamo_registrado.blade.php
@@ -4,14 +4,17 @@
     <title>Reclamo Registrado</title>
 </head>
 <body>
-    <h1>Reclamo Registrado</h1>
+    <h1>Reclamo Registrado R-B</h1>
     <p>Estimado/a {{ $persona->nombres_apellidos ?? $persona->razon_social }},</p>
     <p>Su reclamo ha sido registrado correctamente. A continuación, los detalles de su reclamo:</p>
     <ul>
         <li><strong>Tipo de Reclamo:</strong> {{ $reclamo->tipo_reclamo }}</li>
         <li><strong>Bien Contratado:</strong> {{ $reclamo->bien_contratado }}</li>
-        <li><strong>Texto de Reclamo:</strong> {{ $reclamo->texto_reclamo }}</li>
-        <li><strong>Texto de Queja:</strong> {{ $reclamo->texto_queja }}</li>
+        @if($reclamo->tipo_reclamo === 'reclamo')
+            <li><strong>Texto de Reclamo:</strong> {{ $reclamo->texto_reclamo }}</li>
+        @elseif($reclamo->tipo_reclamo === 'queja')
+            <li><strong>Texto de Queja:</strong> {{ $reclamo->texto_queja }}</li>
+        @endif
         <li><strong>Detalle de Reclamación:</strong> {{ $reclamo->detalle_reclamacion }}</li>
     </ul>
     <p>Gracias por su atención.</p>
